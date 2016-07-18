e1a5b768eb6c6b38079dbcf135cfc0ccdff843bb
Meli Amelia <ukhti.meli15@gmail.com>
2016-07-18 17:43:05 +0700

    Update VeditMember.php

--- VeditMember.php
+++ VeditMember.php
@@ -2,13 +2,21 @@
 	<title>Add Member</title>
 		<head></head>
 			<body>
-			<form method="post" name="add_member" action="<?php echo base_url();?>Cmember/add_member">
+			<form method="post" name="edit_member" action="<?php echo base_url();?>Cmember/edit_memberOne">
 				<table>
 					<?php foreach($member_edit as $record){ ?>
 					<tr>
 						<td>name</td>
 						<td></td>
-						<td><input type="text" name="name_member" value="<?php echo $record->name; ?>"></td>
+						<td><input type="hidden" name="id" value="<?php echo $record->id;?>"><input type="text" name="name_member" value="<?php echo $record->name; ?>"></td>
+					</tr>
+					<tr>
+						<td>Sex</td>
+						<td></td>
+						<td>
+							<input type="radio" id="M" name="sex" value="M" <?php echo set_radio('sex', 'M', $record->sex == 'M'); ?>  />Male</label>
+							<input type="radio" id="F" name="sex" value="F" <?php echo set_radio('sex', 'F', $record->sex == 'F'); ?>  />Female</label>
+						</td>
 					</tr>
 					<tr>
 						<td>Address</td>
