3d0a66f8772dcdd08bed4106e533bdfd6d5752e3
Meli Amelia <ukhti.meli15@gmail.com>
2016-07-18 17:43:36 +0700

    Update Vmember.php

--- Vmember.php
+++ Vmember.php
@@ -3,25 +3,36 @@
 	<head></head>
 	<body>
 		<h1>My Member</h1>
-		<a href="<?php echo base_url();?>Cmember/member">Add Member</a>
+		<a href="<?php echo base_url();?>Cmember/form_member">Add Member</a>
 		<table>
 			<tr>
 				<td>No</td>
 				<td>Name</td>
+				<td>Sex</td>
 				<td>Address</td>
 				<td>Action</td>
 			</tr>
 			<?php
 				$no = 1;
 				foreach($member as $record){
+				$sex = $record->sex;
 			?>
 			<tr>
 				<td><?php echo $no;?></td>
 				<td><?php echo $record->name;?></td>
+				<td>
+					<?php
+						switch($sex){
+						case 'M' : echo "Male";break;
+						case 'F' : echo "Female";break;
+						}
+					?>
+				</td>
+				
 				<td><?php echo $record->address;?></td>
 				<td>
-					<a href="<?php echo base_url();?>Cmember/edit_member">Edit</a>
-					<a href="<?php echo base_url();?>Cmember/delete_member ">Delete</a>
+					<a href="<?php echo base_url();?>Cmember/edit_member/<?php echo $record->id;?>">Edit</a>
+					<a href="<?php echo base_url();?>Cmember/delete_member/<?php echo $record->id;?>">Delete</a>
 				</td>
 			</tr>
 			<?php $no++; }?>
